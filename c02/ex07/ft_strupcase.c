c41e9aa716971f13e394facc1bef7485e441bef0
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-11 22:19:45 +0400

    ex07

---
+++ c02/ex07/ft_strupcase.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 21:56:02 by aramarak          #+#    #+#             */
+/*   Updated: 2024/09/11 22:08:33 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+char	*ft_strupcase(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
+/*
+int	main(void)
+{
+	char	text[] = "text";
+	printf("%s", ft_strupcase(text));
+	return (0);
+}*/
