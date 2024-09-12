a1fa5822389d4788dfa0d575f03f45a3fd909cbd
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-12 20:40:24 +0400

    add: ex08

---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 19:57:46 by aramarak          #+#    #+#             */
+/*   Updated: 2024/09/12 20:38:50 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+char	*ft_strlowcase(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+/*
+int	main(void)
+{
+	char str[] = "ASDFGHJKL";
+	printf("%s", ft_strlowcase(str));
+	return (0);
+}*/
