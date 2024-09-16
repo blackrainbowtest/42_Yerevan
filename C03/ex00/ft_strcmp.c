0b61bc81838d8585fb8360bedcc7c096ab7b4fb7
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-16 21:14:30 +0400

    add: ex00

---
+++ C03/ex00/ft_strcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 11:48:22 by aramarak          #+#    #+#             */
+/*   Updated: 2024/09/14 12:38:50 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d", ft_strcmp("test", "test22"));
+	return (0);
+}*/
