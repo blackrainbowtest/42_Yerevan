8d9aedb7656194491b3d52bc2733da0696275e0b
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-17 15:59:10 +0400

    add : final

--- Rush01/ex02/main.c
+++ Rush01/ex02/main.c
@@ -21,6 +21,10 @@ int	ft_check_cell(int **matrix, int *pos, int **arr, int num)
 	{
 		if (matrix[pos[0]][nb] == num || matrix[nb][pos[1]] == num)
 		{
+			if (matrix[3][0] == 3 && matrix[0][0] == 2 && matrix[1][0] == 4)
+			{
+				printf("num:%d\n", num);
+			}
 			matrix[pos[0]][pos[1]] = 0;
 			return (0);
 		}
@@ -29,17 +33,24 @@ int	ft_check_cell(int **matrix, int *pos, int **arr, int num)
 	matrix[pos[0]][pos[1]] = num;
 	if (matrix[3][0] == 3 && matrix[0][0] == 2 && matrix[1][0] == 4)
 	{
-	// printf("Validation updown: %d\n", ft_check_l_ver_vision(matrix, pos[0], arr[2]));
-	// printf("Validation updown: %d\n", ft_check_r_ver_vision(matrix, pos[0], arr[3]));
+	// printf("Validation left: %d\n", ft_check_l_hor_vision(matrix, pos[0], arr[2]));
+	// printf("Validation right: %d\n", ft_check_r_hor_vision(matrix, pos[0], arr[3]));
 	ft_display_matrix(matrix, 4, 4);
+	printf("current_Value: %d\n\n", matrix[pos[0]][pos[1]]);
 	}
 	valid = 1;
 	valid &= ft_check_u_ver_vision(matrix, pos[1], arr[0]);
 	valid &= ft_check_d_ver_vision(matrix, pos[1], arr[1]);
 	valid &= ft_check_l_hor_vision(matrix, pos[0], arr[2]);
 	valid &= ft_check_r_hor_vision(matrix, pos[0], arr[3]);
+		if (matrix[3][0] == 3 && matrix[0][0] == 2 && matrix[1][0] == 4)
+		{
+		printf("current_Value: %d\n\n", matrix[pos[0]][pos[1]]);
+		}
 	if (!valid)
+	{
 		matrix[pos[0]][pos[1]] = 0;
+	}
 	return (valid);
 }
 
@@ -51,13 +62,13 @@ int	ft_run_loop(int **matrix, int **arr, int row, int col)
 
 	nb = 1;
 	found_solution = 0;
-	if (row == 4)
-		return (1);
 	if (col == 4)
 	{
 		row++;
 		col = 0;
 	}
+	if (row == 4)
+		return (1);
 	while (nb <= 4)
 	{
 		pos[0] = row;
@@ -67,7 +78,7 @@ int	ft_run_loop(int **matrix, int **arr, int row, int col)
 			matrix[row][col] = nb;
 			if (ft_run_loop(matrix, arr, row, col + 1))
 			{
-				found_solution = 1;
+				found_solution += 1;
 				break;
 			}
 			matrix[row][col] = 0;
--- Rush01/ex02/valid.c
+++ Rush01/ex02/valid.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+/*
+if not 0 we check row else col/up dows/
+*/
+int	ft_is_full(int pos1, int type, int **matrix)
+{
+	int		nb;
+
+	nb = 0;
+	while (nb < 4)
+	{
+		if (type)
+			if (matrix[pos1][nb] == 0)
+				return (0);
+		else
+			if (matrix[nb][pos1] == 0)
+				return (0);
+		nb++;
+	}
+	return (1);
+}
+
 int	ft_check_u_ver_vision(int **matrix, int column, int *limits)
 {
 	int		last_max;
@@ -18,6 +39,8 @@ int	ft_check_u_ver_vision(int **matrix, int column, int *limits)
 		}
 		nb++;
 	}
+	if (!ft_is_full(column, 0, matrix));
+		return (1);
 	return (vision_count <= limits[column]);
 }
 
@@ -26,10 +49,12 @@ int	ft_check_d_ver_vision(int **matrix, int column, int *limits)
 	int		last_max;
 	int		vision_count;
 	int		nb;
+	int		is_row_full;
 
 	last_max = 0;
 	vision_count = 0;
 	nb = 3;
+	is_row_full = ft_is_full(column, 0, matrix);
 	while (nb >= 0)
 	{
 		if (matrix[nb][column] > last_max)
@@ -37,8 +62,14 @@ int	ft_check_d_ver_vision(int **matrix, int column, int *limits)
 			last_max = matrix[nb][column];
 			vision_count++;
 		}
+		if (matrix[3][0] == 3 && matrix[0][0] == 2 && matrix[1][0] == 4)
+		{
+			printf("is_row_full - %d\n", is_row_full);
+		}
 		nb--;
 	}
+	if (!is_row_full)
+		return (1);
 	return (vision_count <= limits[column]);
 }
 
@@ -47,10 +78,12 @@ int	ft_check_l_hor_vision(int **matrix, int row, int *limits)
 	int		last_max;
 	int		vision_count;
 	int		nb;
+	int		is_row_full;
 
 	last_max = 0;
 	vision_count = 0;
 	nb = 0;
+	is_row_full = ft_is_full(row, 1, matrix);
 	while (nb < 4)
 	{
 		if (matrix[row][nb] > last_max)
@@ -60,12 +93,12 @@ int	ft_check_l_hor_vision(int **matrix, int row, int *limits)
 		}
 		if (matrix[3][0] == 3 && matrix[0][0] == 2 && matrix[1][0] == 4)
 		{
-			printf("FROM LEFT - %d\n", matrix[row][nb]);
-			printf("vision_count - %d, limits - %d \n", vision_count, limits[row]);
-			// printf("");
+			printf("is_row_full - %d\n", is_row_full);
 		}
 		nb++;
 	}
+	if (!is_row_full)
+		return (1);
 	return (vision_count <= limits[row]);
 }
 
@@ -74,10 +107,12 @@ int	ft_check_r_hor_vision(int **matrix, int row, int *limits)
 	int		last_max;
 	int		vision_count;
 	int		nb;
+	int		is_row_full;
 
 	last_max = 0;
 	vision_count = 0;
 	nb = 3;
+	is_row_full = ft_is_full(row, 1, matrix);
 	while (nb >= 0)
 	{
 		if (matrix[row][nb] > last_max)
@@ -87,11 +122,11 @@ int	ft_check_r_hor_vision(int **matrix, int row, int *limits)
 		}
 		if (matrix[3][0] == 3 && matrix[0][0] == 2 && matrix[1][0] == 4)
 		{
-			printf("FROM LEFT - %d\n", matrix[row][nb]);
-			printf("vision_count - %d, limits - %d \n", vision_count, limits[row]);
-			// printf("");
+			printf("is_row_full - %d - %d\n", is_row_full, last_max);
 		}
 		nb--;
 	}
+	if (!is_row_full)
+		return (1);
 	return (vision_count <= limits[row]);
 }
