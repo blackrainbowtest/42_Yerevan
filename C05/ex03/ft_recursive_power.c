01b2bf9d8560e79b49353528cddc211668e98276
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-18 13:48:37 +0400

    add: ex03

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,20 @@
+int	ft_recursive_power(int nb, int power)
+{
+	int		result;
+
+	result = nb;
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
+	if (power > 1)
+		return (nb * ft_recursive_power(nb, (power - 1)));
+	return (resule);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d", ft_recursive_power(2, 2));
+	return (0)
+}*/
