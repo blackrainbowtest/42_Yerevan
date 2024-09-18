b16aff56d94df30b7f1b7a8d12d9fc63f37ad7cf
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-18 13:49:17 +0400

    add: ex05

---
+++ C05/ex06/ft_is_prime.c
@@ -0,0 +1,26 @@
+int	ft_is_prime(int nb)
+{
+	int		i;
+
+	if (nb < 2)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	i = 3;
+	while (i <= nb / 2)
+	{
+		if (nb % i == 0)
+			return (0);
+		i += 2;
+	}
+	return (1);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d", ft_is_prime(7));
+	return (0);
+}*/
