50643670cefcb07f5bf77f23c3a22e0016adac4a
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-18 19:46:10 +0400

    modified: 03

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -5,32 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/17 20:41:41 by aramarak          #+#    #+#             */
-/*   Updated: 2024/09/17 20:41:43 by aramarak         ###   ########.fr       */
+/*   Created: 2024/09/18 19:26:28 by aramarak          #+#    #+#             */
+/*   Updated: 2024/09/18 19:27:38 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-    int     res;
-    int     sign;
+	int		res;
+	int		sign;
 
-    sign = 1;
-    res = 0;
-    while (*str == ' ' || (*str >= 9 && *str <= 13))
-        str++;
-    while (*str == 43 || *str == 45)
-    {
-        if (*str == 45)
-            sign *= -1;
-        str++;
-    }
-    while (*str >= 48 && *str <= 57)
-    {
-        res = res * 10 + (*str - 48);
-        str++;
-    }
-    return (res * sign);
+	sign = 1;
+	res = 0;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == 43 || *str == 45)
+	{
+		if (*str == 45)
+			sign *= -1;
+		str++;
+	}
+	while (*str >= 48 && *str <= 57)
+	{
+		res = res * 10 + (*str - 48);
+		str++;
+	}
+	return (res * sign);
 }
 /*
 #include <stdio.h>
