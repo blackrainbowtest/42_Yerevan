dfadfd0c6ce501e9eddaf24bd99f5478ec651c64
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-19 19:32:15 +0400

    modified: fix norm error

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 19:55:54 by aramarak          #+#    #+#             */
+/*   Updated: 2024/09/18 19:57:07 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_fibonacci(int index)
 {
 	if (index < 0)
@@ -6,9 +18,9 @@ int	ft_fibonacci(int index)
 		return (index);
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
-
+/*
 int	main(void)
 {
 	printf("%d", ft_fibonacci(5));
 	return (0);
-}
+}*/
