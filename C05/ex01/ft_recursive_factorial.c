7d7d727326d6636349c8f8e7b901986bd8887dde
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-19 19:31:53 +0400

    modified: fix norm error

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -1,15 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 19:48:46 by aramarak          #+#    #+#             */
+/*   Updated: 2024/09/18 19:49:30 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_recursive_factorial(int nb)
 {
 	if (nb == 0)
 		return (1);
 	if (nb < 0)
 		return (0);
-	return nb * ft_recursive_factorial(nb - 1);
+	return (nb * ft_recursive_factorial(nb - 1));
 }
-
+/*
 #include <stdio.h>
+
 int	main(void)
 {
 	printf("%d", ft_recursive_factorial(9));
 	return (0);
-}
+}*/
