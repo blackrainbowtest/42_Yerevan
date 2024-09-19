820dcb12f22db7968f2c84e768b80f7d70148cd1
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-19 19:32:00 +0400

    modified: fix norm error

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -1,6 +1,18 @@
-int	ft_iterative_power
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 19:49:59 by aramarak          #+#    #+#             */
+/*   Updated: 2024/09/18 20:34:40 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
 {
-	int		result;
+	int	result;
 
 	result = 1;
 	if (power == 0)
@@ -13,6 +25,7 @@ int	ft_iterative_power
 }
 /*
 #include <stdio.h>
+
 int	main(void)
 {
 	printf("%d", ft_iterative_power(3, 4));
