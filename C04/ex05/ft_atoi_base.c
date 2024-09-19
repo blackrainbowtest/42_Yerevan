d54623a31f60f12ed7d72cb29c487ae3527e93c3
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-19 12:46:52 +0400

    modified: fixed bugs

--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -1,7 +1,7 @@
 int	ft_strlng(char *str)
 {
 	int		i;
-	
+
 	i = 0;
 	while (str[i])
 		i++;
@@ -15,15 +15,16 @@ int	ft_str_check(char *str)
 	int		len;
 
 	i = 0;
-	len = ft_string;
+	len = ft_strlng(str);
 	if (len < 2)
 		return (0);
 	while (str[i])
 	{
-		if (str[i] <= 32 || str[i] == 43 || str[i] == 45)
+		if (str[i] == '+' || str[i] == '-'
+			|| str[i] > 126 || str[i] <= ' ' )
 			return (0);
 		j = i + 1;
-		while (str[j] != '\0')
+		while (str[j])
 		{
 			if (str[i] == str[j])
 				return (0);
@@ -48,23 +49,54 @@ int	ft_get_base(char str, char *base)
 	return (-1);
 }
 
+int	ft_base_sign(char *str, int *p_start_from)
+{
+	int		i;
+	int		sign;
 
+	i = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	*p_start_from = i;
+	return (sign);
+}
 
 int	ft_atoi_base(char *str, char *base)
 {
-	int		i;
+	int		start_from;
 	int		sign;
 	int		nb;
 	int		nb2;
 	int		base_lng;
 
 	nb = 0;
-	i = 0;
-
-}
-
-int	main(void)
-{
-	printf("%d\n", ft_atoi_base());
+	start_from = 0;
+	base_lng = ft_str_check(base);
+	if (base_lng)
+	{
+		sign = ft_base_sign(str, &start_from);
+		nb2 = ft_get_base(str[start_from], base);
+		while (nb2 != -1)
+		{
+			nb = (nb * base_lng) + nb2;
+			start_from++;
+			nb2 = ft_get_base(str[start_from], base);
+		}
+		return (nb *= sign);
+	}
 	return (0);
 }
+/*
+int	main(void)
+{
+	printf("%d\n", ft_atoi_base("1010", "-01"));
+	printf("%d\n", ft_atoi_base("-2FF", "0123456789ABCDEF"));
+	return (0);
+}*/
