806b6f856d3d19addeba8f9f4b5e264510fe5fdd
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-19 21:17:36 +0400

    modified: update

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 19:55:28 by aramarak          #+#    #+#             */
-/*   Updated: 2024/09/18 19:55:32 by aramarak         ###   ########.fr       */
+/*   Updated: 2024/09/19 20:56:09 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ int	ft_recursive_power(int nb, int power)
 		return (0);
 	if (power > 1)
 		return (nb * ft_recursive_power(nb, (power - 1)));
-	return (resule);
+	return (result);
 }
 /*
 #include <stdio.h>
 int	main(void)
 {
 	printf("%d", ft_recursive_power(2, 2));
-	return (0)
+	return (0);
 }*/
