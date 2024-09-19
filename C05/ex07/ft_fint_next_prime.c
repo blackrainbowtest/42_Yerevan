a898f4565cac7de31274a071b683a93218674c0f
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-19 21:17:42 +0400

    modified: update

--- C05/ex07/ft_fint_next_prime.c
+++ C05/ex07/ft_fint_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 20:01:32 by aramarak          #+#    #+#             */
-/*   Updated: 2024/09/18 20:02:29 by aramarak         ###   ########.fr       */
+/*   Updated: 2024/09/19 21:13:40 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_is_prime(int nb)
 	if (nb % 2 == 0)
 		return (0);
 	i = 3;
-	while (i <= (nb / 2))
+	while (i * i <= nb)
 	{
 		if (nb % i == 0)
 			return (0);
@@ -36,10 +36,10 @@ int	ft_find_next_prime(int nb)
 		nb++;
 	return (nb);
 }
-/*
+
 #include <stdio.h>
 int	main(void)
 {
 	printf("%d\n", ft_find_next_prime(7));
 	return (0);
-}*/
+}
