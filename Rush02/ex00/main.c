a223a8c5e749f23b7b3c00dc0e8345ad26415e44
Aram <araqelyan.aram.96@gmail.com>
2024-09-22 01:09:08 +0400

    modified: main.c

--- Rush02/ex00/main.c
+++ Rush02/ex00/main.c
@@ -3,16 +3,21 @@
 int	main(int argc, char **argv) {
 	if (argc < 2 || argc > 3) {
 		ft_putstr("Error\n");
-		return 1;
+		return (1);
 	}
 	if (argc == 2)
 	{
 		if (ft_core(argv[1], "numbers.dict"))
 		{
-			
+			ft_putstr("Error\n");
+			return (1);
 		}
 	} else if (argc == 3) {
-		ft_core(argv[1], argv[2]);
+		if (ft_core(argv[1], argv[2]))
+		{
+			ft_putstr("Error\n");
+			return (1);
+		}
 	}
-	return 0;
+	return (0);
 }
