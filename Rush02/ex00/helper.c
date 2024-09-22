72e96db7583ad79746591d7d5a8ebb341643079d
Aram <araqelyan.aram.96@gmail.com>
2024-09-22 22:58:46 +0400

    add

--- Rush02/ex00/helper.c
+++ Rush02/ex00/helper.c
@@ -5,10 +5,10 @@ void	ft_print_three_helper(char *str, int count, int *is_first)
 {
 	char	**dict_data;
 	int		i;
-    int     is_exit;
+	int		is_exit;
 
-	i = 0;
-    is_exit = 1;
+	i = -1;
+	is_exit = 1;
 	dict_data = static_save(NULL);
 	if (str[0] == '0')
 	{
@@ -17,7 +17,7 @@ void	ft_print_three_helper(char *str, int count, int *is_first)
 	}
 	else
 	{
-		while (dict_data[i] && is_exit)
+		while (dict_data[++i] && is_exit != 1)
 		{
 			if (dict_data[i][0] == str[0] && dict_data[i][1] == ':')
 			{
@@ -25,45 +25,48 @@ void	ft_print_three_helper(char *str, int count, int *is_first)
 				ft_get_tail(0);
 				is_exit = 0;
 			}
-			i++;
 		}
 		ft_print_two(++str, count, 0);
 	}
 }
 
-void    ft_process_teen_digits(char *str)
+void	ft_process_teen_digits(char *str)
 {
 	int		i;
 	char	**dict_data;
+	int		is_exit;
 
 	dict_data = static_save(NULL);
 	i = 0;
-	while (dict_data[i])
+	is_exit = 1;
+	while (dict_data[i] && is_exit != 1)
 	{
 		if (dict_data[i][0] == str[0] && dict_data[i][1] == str[1])
 		{
 			ft_get_word(i);
-			break;
+			is_exit = 0;
 		}
 		i++;
 	}
 }
 
-void    ft_process_tens_digits(char *str, int count)
+void	ft_process_tens_digits(char *str, int count)
 {
 	int		i;
 	char	**dict_data;
+	int		is_exit;
 
 	dict_data = static_save(NULL);
 	i = 0;
-	while (dict_data[i])
+	is_exit = 1;
+	while (dict_data[i] && is_exit != 1)
 	{
 		if (dict_data[i][0] == str[0] && dict_data[i][1] == '0')
 		{
 			ft_get_word(i);
 			if (str[1] != '0')
 				ft_print_one(++str, count, 0);
-			break;
+			is_exit = 0;
 		}
 		i++;
 	}
