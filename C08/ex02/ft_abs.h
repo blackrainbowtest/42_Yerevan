7a3643f1c81278f796960cce61f0025dd6ef927a
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-26 00:18:16 +0400

    add

--- C08/ex02/ft_abs.h
+++ C08/ex02/ft_abs.h
@@ -6,21 +6,13 @@
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 22:05:15 by aramarak          #+#    #+#             */
-/*   Updated: 2024/09/25 22:07:16 by aramarak         ###   ########.fr       */
+/*   Updated: 2024/09/25 23:42:10 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-static inline int	abs_value(int value)
-{
-	if (value < 0)
-		return (-value);
-	else
-		return (value);
-}
-
-# define ABS(value) (abs_value(value))
+# define ABS(value) (value * ((value > 0) - (value < 0))
 
 #endif
