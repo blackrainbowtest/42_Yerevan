98c1331147967883fe2f33a56d636a2f5713ab7d
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-25 16:47:59 +0400

    modified: ex02

--- C08/ex02/ft_abs.h
+++ C08/ex02/ft_abs.h
@@ -1,6 +1,14 @@
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-# define ABS(n) ((n) < 0 ? -n : n)
+static inline int	abs_value(int value)
+{
+	if (value < 0)
+		return (-value);
+	else
+		return (value);
+}
+
+# define ABS(value) (abs_value(value))
 
 #endif
