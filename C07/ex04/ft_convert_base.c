d5211e6ae2de8c6f30c53b875d0d1469bdbd8e5b
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-09-30 10:16:41 +0400

    modified c07/ex04

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -8,5 +8,6 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 #include <stdio.h>
 int	main(void)
 {
-	printf("$s\n", ft_convert_base("", "", ""))
+	printf("$s\n", ft_convert_base("", "", ""));
+	return (0);
 }
